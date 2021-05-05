461153d17456fe47c311458dd312e84cd89376f6
Cooper Worobetz <cooper@worobetz.ca>
2021-05-05 12:04:22 -0700

    Add systray and icons
    Signed-off-by: Cooper Worobetz <cooper@worobetz.ca>

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/fsnotify/fsnotify v1.4.9
+	github.com/getlantern/systray v1.1.0
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/getlantern/systray"
 	"gopkg.in/yaml.v2"
 )
 
@@ -24,9 +26,43 @@ type Config struct {
 func main() {
 
 	cfg := setup()
+	systray.Run(onReady, onExit)
 
 	log.Printf("Watching for file \"%s\", will move to \"%s\"", cfg.Watch.Path, cfg.Destination.Path)
 	watch(cfg)
+}
+
+func getIcon(path string) []byte {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Error opening icon file: %s", err)
+	}
+	return b
+}
+
+func onReady() {
+
+	// Main systray icon
+	systray.SetIcon(getIcon("assets/pencil.ico"))
+	systray.SetTitle("Tiddly Saver")
+	systray.SetTooltip("Tiddly Saver - Download your Tiddlywiki somewhere else")
+
+	// Tooltips
+	setSystrayMenuItem("Exit", "Shutdown the app")
+}
+
+func setSystrayMenuItem(title string, tooltip string) {
+	mExit := systray.AddMenuItem(title, tooltip)
+
+	go func() {
+		<-mExit.ClickedCh
+		onExit()
+	}()
+}
+
+func onExit() {
+	log.Printf("Exit signal received, stopping program.")
+	os.Exit(0)
 }
 
 func setup() Config {
@@ -60,6 +96,7 @@ func setup() Config {
 	}
 	cfg.Destination.Path = abs
 
+	log.Printf("Setup complete. %+v", cfg)
 	return cfg
 }
 
@@ -88,7 +125,8 @@ func watch(cfg Config) {
 				}
 				// If it's a write event and the name of the events file matches the file to watch
 				if event.Op&fsnotify.Write == fsnotify.Write && event.Name == cfg.Watch.Path {
-					log.Printf("Resetting move timer")
+					// TODO - change message if starting or restarting timer
+					log.Printf("Change to \"%s\" detected, starting wait period of %d seconds", cfg.Watch.Path, cfg.Wait)
 					timer.Reset(duration)
 				}
 			case err, ok := <-watcher.Errors:
